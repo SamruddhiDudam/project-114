149934d061c2b9f397691f14adc3cd0efd0fe773
SamruddhiDudam <78253578+SamruddhiDudam@users.noreply.github.com>
2021-08-19 18:16:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,15 +3,16 @@ function preload() {
   }
   
 function setup() {
-  canvas = createCanvas(640,480);
+  canvas = createCanvas(300,300);
   canvas.center();
   video = createCapture(VIDEO);
-  video.size(640,480);
+  video.size(300,300);
   video.hide();
-  }
+}
   
-function draw() {
 
+function draw() {
+image(video,0,0,640,480);
   }
   
 function take_snapshot(){    
